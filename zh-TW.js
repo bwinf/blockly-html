681bbf108749e26b2772ad613a3488666bbf4d6b
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-30 07:59:07 +0800

    s

---
+++ zh-TW.js
@@ -0,0 +1,289 @@
+Msg = {
+  categories: {
+    "html": "HTML",
+    "baseframe": "基本架構",
+    "textstructure": "文字架構",
+    "markup": "文字表示",
+    "style": "樣表",
+    "enumerations": "清單",
+    "tables": "表格",
+    "forms": "表單",
+    "scripts": "程式",
+    "scripting": "程式碼",
+    "logic": "邏輯",
+    "loops": "迴圈",
+    "math": "數學",
+    "text": "文字",
+    "lists": "串列",
+    "colour": "顏色",
+    "variables": "變數",
+    "functions": "函式",
+  },
+  blocks: {
+    "baseframe": {
+      "message0": "文件 %1 頭部 %2 %3 內容 %4 %5",
+    },
+    "html": {
+      "message0": "文件 %1 %2",
+    },
+    "body": {
+      "message0": "內容 %1 %2",
+    },
+    "head": {
+      "message0": "頭部 %1 %2",
+    },
+    "title": {
+      "message0": "標題 %1",
+    },
+    "paragraph": {
+      "message0": "段茖 %1",
+    },
+    "plaintext": {
+      "message0": "文字 %1",
+    },
+    "division": {
+      "message0": "區塊 %1 %2",
+    },
+    "style": {
+      "message0": "樣式 =  %1 %2",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "color": {
+      "message0": "文字顏色 :  %1",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "bgcolour": {
+      "message0": "背景顏色 :  %1",
+      "helpUrl": ""
+    },
+    "genericstyle": {
+      "message0": "%1 : %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "屬性"},
+        {"text": "值"}
+      ],
+    },
+    "generictag": {
+      "message0": "< %1 > %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "tag"},],
+    },
+    "more_attributes": {
+      "message0": "%1 %2 %3",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "genericattribute": {
+      "message0": "%1  =  %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "屬性"},
+        {"text": "值"}
+      ],
+    },
+    "link": {
+      "message0": "link to %1 %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "target"}],
+    },
+    "span": {
+      "message0": "區段 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "image": {
+      "message0": "圖片 %1 或 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [
+        {"text": "網址"},
+        {"text": "替代文字"}
+      ],
+    },
+    "emphasise": {
+      "message0": "強調 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "strong": {
+      "message0": "重要的 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "headline": {
+      "message0": "標頭 %1 %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"options": [
+        ["level 1","h1"],
+        ["level 2","h2"],
+        ["level 3","h3"],
+        ["level 4","h4"],
+        ["level 5","h5"],
+        ["level 6","h6"]
+      ]}],
+    },
+    "linebreak": {
+      "message0": "分行",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "horizontalbreak": {
+      "message0": "分隔線",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "unorderedlist": {
+      "message0": "齊一清單 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "orderedlist": {
+      "message0": "有序清單 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "listelement": {
+      "message0": "清單項目 %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+
+    "inserted": {
+      "message0": "插入在 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/ins"
+    },
+
+    "deleted": {
+      "message0": "已刪除 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/del"
+    },
+
+    "super": {
+      "message0": "上標 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/sup"
+    },
+
+    "sub": {
+      "message0": "下標 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/sub"
+    },
+
+    "code": {
+      "message0": "程式碼 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/code"
+    },
+
+    "quote": {
+      "message0": "引用 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/q"
+    },
+
+    "blockquote": {
+      "message0": "引用區塊 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/blockquote"
+    },
+
+    "sample": {
+      "message0": "範例 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/samp"
+    },
+
+    "keyboard": {
+      "message0": "鍵盤 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/kbd"
+    },
+
+    "variable": {
+      "message0": "變數 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/var"
+    },
+
+    "form": {
+      "message0": "表單 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/form"
+    },
+
+    "table": {
+      "message0": "表格 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/table"
+    },
+
+    "tablerow": {
+      "message0": "列 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/tr"
+    },
+
+    "tablecell": {
+      "message0": "儲存格 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/td"
+    },
+
+    "input_text": {
+      "message0": "輸入文字 %1",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/input"
+    },
+
+    "button": {
+      "message0": "按鈕 %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/button"
+    },
+
+    "input": {
+      "message0": "輸入 %2 %3",
+      "args0": [{ "options": [
+        ["Text", "文字"],
+        ["E-Mail", "email"],
+        ["Zahl", "數字"],
+        ["Passwort", "密碼"],
+        ["Checkbox", "勾選"],
+        ["Radiobutton", "擇一勾選"],
+        ["Button", "按鈕"],
+        ["Farbe", "顏色"],
+        ["Datum", "日期"],
+        ["Lokalzeit", "本地日期時間"],
+        ["Datei", "檔案"],
+        ["Versteckt", "隱藏"],
+        ["Bild", "圖片"],
+        ["Monat", "月"],
+        ["Bereich", "範圍"],
+        ["Zurücksetzen", "重設"],
+        ["Suche", "搜尋"],
+        ["Abschicken", "登錄"],
+        ["Telefonnummer", "電話"],
+        ["Zeit", "時間"],
+        ["URL", "網址"],
+        ["Woche", "週"]]
+      }],
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/input"
+    }
+  }
+}
+
+
+
+
